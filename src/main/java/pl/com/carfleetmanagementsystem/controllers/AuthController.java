e830f7d0146499c9a7faf2b90e826c1afa46d808
Łukasz Oszczypała <lukasz.oszczypala@gmail.com>
2020-04-09 02:31:45 +0200

    auth with jwt working

---
+++ src/main/java/pl/com/carfleetmanagementsystem/controllers/AuthController.java
@@ -0,0 +1,141 @@
+package pl.com.carfleetmanagementsystem.controllers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.com.carfleetmanagementsystem.models.ERole;
+import pl.com.carfleetmanagementsystem.models.Role;
+import pl.com.carfleetmanagementsystem.models.User;
+import pl.com.carfleetmanagementsystem.http.request.LoginRequest;
+import pl.com.carfleetmanagementsystem.http.request.SignupRequest;
+import pl.com.carfleetmanagementsystem.http.response.JwtResponse;
+import pl.com.carfleetmanagementsystem.http.response.MessageResponse;
+import pl.com.carfleetmanagementsystem.repository.RoleRepository;
+import pl.com.carfleetmanagementsystem.repository.UserRepository;
+import pl.com.carfleetmanagementsystem.security.jwt.JwtUtils;
+import pl.com.carfleetmanagementsystem.security.services.UserDetailsImpl;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_NEW)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "employee":
+                        Role employeeRole = roleRepository.findByName(ERole.ROLE_EMPLOYEE)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(employeeRole);
+
+                        break;
+                    case "boss":
+                        Role bossRole = roleRepository.findByName(ERole.ROLE_BOSS)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(bossRole);
+
+                        break;
+					case "driver":
+						Role driverRole = roleRepository.findByName(ERole.ROLE_DRIVER)
+								.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+						roles.add(driverRole);
+
+						break;
+                    default:
+                        Role newRole = roleRepository.findByName(ERole.ROLE_NEW)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(newRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
