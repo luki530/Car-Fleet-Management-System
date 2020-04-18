7341ac0c71ff896c51278c654f6b45c44fc01398
Łukasz Oszczypała <lukasz.oszczypala@gmail.com>
2020-04-18 15:26:31 +0200

    authorization fix

--- src/main/java/pl/com/carfleetmanagementsystem/controllers/UserController.java
+++ src/main/java/pl/com/carfleetmanagementsystem/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import pl.com.carfleetmanagementsystem.repository.UserRepository;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class UserController {
 
