ab65ebc7d9700ba89766dfee96babc862bed4bc1
Łukasz Oszczypała <lukasz.oszczypala@gmail.com>
2020-04-19 12:06:58 +0200

    myprofile

--- src/main/java/pl/com/carfleetmanagementsystem/controllers/UserController.java
+++ src/main/java/pl/com/carfleetmanagementsystem/controllers/UserController.java
@@ -1,16 +1,15 @@
 package pl.com.carfleetmanagementsystem.controllers;
 
+import com.sun.mail.iap.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.com.carfleetmanagementsystem.http.response.MessageResponse;
 import pl.com.carfleetmanagementsystem.models.User;
 import pl.com.carfleetmanagementsystem.repository.UserRepository;
+import pl.com.carfleetmanagementsystem.security.jwt.JwtUtils;
 
 import java.util.List;
 
@@ -21,11 +20,22 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    JwtUtils jwtUtils;
+
     @GetMapping("/listofusers")
     //@PreAuthorize("hasRole('EMPLOYEE') or hasRole('BOSS') or hasRole('ADMIN')")
-    public ResponseEntity<List<User>> users(Model model){
+    public ResponseEntity<List<User>> users(){
         List<User> users = userRepository.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/myprofile")
+    public ResponseEntity<User> user(@RequestHeader(name="Authorization") String token){
+        token=token.substring(7);
+        String username = jwtUtils.getUserNameFromJwtToken(token);
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new IllegalArgumentException());
+        return ResponseEntity.ok().body(user);
     }
 
 }
