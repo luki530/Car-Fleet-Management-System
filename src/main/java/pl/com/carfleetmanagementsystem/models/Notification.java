7006b613c6ae829dcd03243260bfbd5b1daea54b
Michal <zienowit@gmail.com>
2020-05-04 14:48:20 +0200

    notification class

---
+++ src/main/java/pl/com/carfleetmanagementsystem/models/Notification.java
@@ -0,0 +1,45 @@
+package pl.com.carfleetmanagementsystem.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id")
+    private Car car;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id")
+    private User driver;
+
+    public Notification() {
+    }
+
+    public Notification(Long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
