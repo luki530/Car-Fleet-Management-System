4720b21ec6623dcec93202d1619d2b4607281898
Michal <zienowit@gmail.com>
2020-05-10 22:06:20 +0200

    role repository fixing

---
+++ src/main/java/pl/com/carfleetmanagementsystem/repository/RoleRepository.java
@@ -0,0 +1,19 @@
+package pl.com.carfleetmanagementsystem.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pl.com.carfleetmanagementsystem.models.ERole;
+import pl.com.carfleetmanagementsystem.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+
+    @Override
+    List<Role> findAll();
+
+}
