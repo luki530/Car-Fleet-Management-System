1502dfe169995ecc698ebfda204d94eb7d013ebe
Michal <zienowit@gmail.com>
2020-05-10 22:59:31 +0200

    moze zmiana nazwy pomoze

--- src/main/java/pl/com/carfleetmanagementsystem/controllers/RolesController.java
+++ src/main/java/pl/com/carfleetmanagementsystem/controllers/RolesController.java
@@ -35,50 +35,46 @@ public class RolesController {
         Set<Role> roles = new HashSet<>();
 
         if (strRoles == null) {
-            Role userRole = roleRepository.findByNameIgnoreCase(ERole.ROLE_NEW)
+            Role userRole = roleRepository.findByName(ERole.ROLE_NEW)
                     .orElseThrow(() -> new RoleNotFoundException(ERole.ROLE_NEW.toString()));
             roles.add(userRole);
         } else {
             strRoles.forEach(role -> {
                 switch (role) {
                     case "ROLE_ADMIN":
-                        Role adminRole = roleRepository.findByNameIgnoreCase(ERole.ROLE_ADMIN)
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
                                 .orElseThrow(() -> new RoleNotFoundException(role));
                         roles.add(adminRole);
 
                         break;
                     case "ROLE_EMPLOYEE":
-                        Role employeeRole = roleRepository.findByNameIgnoreCase(ERole.ROLE_EMPLOYEE)
+                        Role employeeRole = roleRepository.findByName(ERole.ROLE_EMPLOYEE)
                                 .orElseThrow(() -> new RoleNotFoundException(role));
                         roles.add(employeeRole);
 
                         break;
                     case "ROLE_BOSS":
-                        Role bossRole = roleRepository.findByNameIgnoreCase(ERole.ROLE_BOSS)
+                        Role bossRole = roleRepository.findByName(ERole.ROLE_BOSS)
                                 .orElseThrow(() -> new RoleNotFoundException(role));
                         roles.add(bossRole);
 
                         break;
                     case "ROLE_DRIVER":
-                        Role driverRole = roleRepository.findByNameIgnoreCase(ERole.ROLE_DRIVER)
+                        Role driverRole = roleRepository.findByName(ERole.ROLE_DRIVER)
                                 .orElseThrow(() -> new RoleNotFoundException(role));
                         roles.add(driverRole);
 
                         break;
                     default:
-                        Role newRole = roleRepository.findByNameIgnoreCase(ERole.ROLE_NEW)
+                        Role newRole = roleRepository.findByName(ERole.ROLE_NEW)
                                 .orElseThrow(() -> new RoleNotFoundException(role));
                         roles.add(newRole);
                 }
             });
         }
-        Role userRole = roleRepository.findByNameIgnoreCase(ERole.ROLE_NEW)
+        Role userRole = roleRepository.findByName(ERole.ROLE_NEW)
                 .orElseThrow(() -> new RoleNotFoundException(ERole.ROLE_NEW.toString()));
         roles.add(userRole);
-
-        if(strRoles.isEmpty()){
-            System.out.println("Google cloud");
-        }
 
         user.setRoles(roles);
         return ResponseEntity.ok().body(userRepository.save(user));
