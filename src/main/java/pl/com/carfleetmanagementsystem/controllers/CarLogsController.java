4c77e346e5d92067979de92367e1614e58354e9f
Michal <zienowit@gmail.com>
2020-05-10 16:25:49 +0200

    o

--- src/main/java/pl/com/carfleetmanagementsystem/controllers/CarLogsController.java
+++ src/main/java/pl/com/carfleetmanagementsystem/controllers/CarLogsController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//import pl.com.carfleetmanagementsystem.analyse.CarAnalysersManager;
 import pl.com.carfleetmanagementsystem.analyse.CarAnalysersManager;
 import pl.com.carfleetmanagementsystem.errorhandler.exception.CarNotFoundException;
 import pl.com.carfleetmanagementsystem.models.CarLog;
